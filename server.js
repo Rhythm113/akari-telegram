2ef6b20888ffadcb1f3e75cc107b77c4e29dbc15
Mahbub Anam <53085722+Rhythm113@users.noreply.github.com>
2022-03-18 01:42:00 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const { Telegraf, Markup } = require('telegraf')
+
+//const bot = new Telegraf("1370810534:AAFL0jZUnzmC7_muqiiT2L-_5wDMItqIziA");
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, "hello there! I'm Akari. Mechine Learning AI ChatBOT. I'm created by @InfinityCrestors1.", {
+    })
+})
+
+const axios = require('axios')
+
+
+
+bot.on('message', (ctx) => axios
+.get('https://www.kukiapi.xyz/api/akari/' + ctx.message.from.first_name + '/message=' + encodeURIComponent(ctx.message.text))
+.then(res => {
+  ctx.telegram.sendMessage(ctx.message.chat.id, res.data.reply)
+  console.log(`message: ${res.data.reply}`)
+  //console.log(res)
+  console.log(ctx.message)
+})
+.catch(error => {
+  ctx.telegram.sendMessage(ctx.message.chat.id, "Server Error !")
+  console.error(error)
+})
+)
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+
+bot.launch();
