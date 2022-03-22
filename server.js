f83ad7de50469151c54572300d7400ae057db03a
Mahbub Anam <53085722+Rhythm113@users.noreply.github.com>
2022-03-23 00:20:14 +0600

    Maintenece

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const axios = require('axios')
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+//----------Commands-----------------------------//
+
 bot.command('start', ctx => {
     console.log(ctx.from)
     bot.telegram.sendMessage(ctx.chat.id, "hello there! I'm Akari. Mechine Learning AI ChatBOT. I'm created by @Rhythm113.", {
@@ -15,7 +17,8 @@ bot.command('id', ctx => {
     })
 })
 
-
+//------------------------API-----------------------//
+/*
 bot.on('message', (ctx) => axios
 .get('https://www.kukiapi.xyz/api/akari/' + encodeURI(ctx.message.from.first_name)  + `/message="${encodeURI(ctx.message.text)}"`)
 .then(res => {
@@ -29,7 +32,11 @@ bot.on('message', (ctx) => axios
   ctx.telegram.sendMessage(ctx.message.chat.id, "Invalid Command or Internal Error")
   console.log(error)
 })
-)
+) */
+//--------Break------------------------------------------------------//
+bot.on('message', (ctx) =>   ctx.telegram.sendMessage(ctx.message.chat.id, "BOT is under Maintenence. It will be back soon :)").catch((err) => console.log(err))
+
+//--------------------------------------------------------------------//
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 const http = require('http');
 //Listener
@@ -37,16 +44,18 @@ const requestListener = function (req, res) {
   res.writeHead(200);
   res.end('BOT Up & Running..');
 }
+//------------------------Listener-----------------------------------//
 
 const server = http.createServer(requestListener);
 server.listen(process.env.PORT || 8080);
 console.log("Server Running")
-//Launch
+//--------------Engine----------------------------------------------//
 bot.launch()
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
-//Keep Alive the node 
+//-------------------------Pinger----------------------------------//
+//Keep Alive the node
 var http2 = require("http");
 setInterval(function() {
     http.get("http://akari-telegram.herokuapp.com");
